d9206407d55a4c7db095700d12b3907fea9e0be6
tusharrajgithub <tusharrajglobus@gmail.com>
2020-07-24 21:17:46 +0530

    initial commit

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using System;
+using domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext:DbContext
+    {
+
+          public DataContext(DbContextOptions<DataContext> options) : base(options)
+          {  
+          }
+
+          public DbSet<Value> values {get; set;}
+
+          protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+       modelBuilder.Entity<Value>().HasData(
+             new Value{ Id=1,Name="tushar"},
+             new Value {Id=2,Name="raj"}
+    );
+   }
+    }
+}
---
+++ Persistance/Migrations/20200724115243_seedvalue1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class seedvalue1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "tushar" });
+
+            migrationBuilder.InsertData(
+                table: "values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "raj" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "values",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "values",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
